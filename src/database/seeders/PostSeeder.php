e26cb638978beaaa7ef87051bc70f541e1966aa3
shimotaroo <shimotaro.0327@gmail.com>
2021-12-17 15:11:51 +0900

    feat: マイグレーション＆シーダー実装

---
+++ src/database/seeders/PostSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('posts')->insert([
+            [
+                'user_id' => 1,
+                'title' => 'メモ1',
+                'body' => 'メモ1メモ1メモ1メモ1メモ1メモ1メモ1'
+            ],
+            [
+                'user_id' => 1,
+                'title' => 'メモ2',
+                'body' => 'メモ2メモ2メモ2メモ2メモ2メモ2メモ2'
+            ],
+            [
+                'user_id' => 1,
+                'title' => 'メモ3',
+                'body' => 'メモ3メモ3メモ3メモ3メモ3メモ3メモ3'
+            ],
+            [
+                'user_id' => 1,
+                'title' => 'メモ4',
+                'body' => 'メモ4メモ4メモ4メモ4メモ4メモ4メモ4'
+            ],
+            [
+                'user_id' => 1,
+                'title' => 'メモ5',
+                'body' => 'メモ5メモ5メモ5メモ5メモ5メモ5メモ5'
+            ],
+            [
+                'user_id' => 1,
+                'title' => 'メモ6',
+                'body' => 'メモ6メモ6メモ6メモ6メモ6メモ6メモ6'
+            ],
+        ]);
+    }
+}
