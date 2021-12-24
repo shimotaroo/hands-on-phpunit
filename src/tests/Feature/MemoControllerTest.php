1238b94553b50c58a44c7bc033f91bdba75418a2
shimotaroo <shimotaro.0327@gmail.com>
2021-12-24 14:43:22 +0900

    add: テストの設定とそれに伴う修正

--- src/tests/Feature/MemoControllerTest.php
+++ src/tests/Feature/MemoControllerTest.php
@@ -8,12 +8,17 @@ use Tests\TestCase;
 
 class MemoControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
      * 「@test」をつけるとテストメソッドとして認識されるので日本語でテスト内容を記載できる
      * @test
      */
     public function index_一覧画面の表示に成功する(): void
     {
+        // シーダーファイルの実行
+        $this->artisan('db:seed', ['--class' => 'MemoSeeder']);
+
         // GETメソッドでパスにアクセス
         $response = $this->get('/');
 
